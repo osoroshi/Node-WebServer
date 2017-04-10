2c4571969c37105dc33991cee0517571ec7fa135
tim conquest <tim conquest>
2017-04-10 22:22:48 +1000

    proj

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website',
+    ProjectMessage: 'Projects List',
     currentYear: new Date().getFullYear()
   });
 });
@@ -21,6 +34,17 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/Projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
@@ -29,6 +53,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server is up on port ${port}');
+  console.log(`Server is up on port ${port}`);
 });
-    
